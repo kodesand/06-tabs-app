2d4cafd42f5db3a66b5dd3a11b29f2fd8578f431
Raphael Jay Bernardo <73016403+hackwebdev@users.noreply.github.com>
2022-08-15 21:43:40 +0800

    tabs setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import { FaAngleDoubleRight } from 'react-icons/fa'
+
+const url = 'https://course-api.com/react-tabs-project'
+
+const App = () => {
+  return <h2>tabs project setup</h2>
+}
+
+export default App
